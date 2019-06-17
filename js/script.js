7d842b351347d7bbe52be0a9a0728a38ea1486db
Jose Collado <jcoll1020@gmail.com>
2019-06-16 23:46:58 -0400

    update

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const searchContainer = $('.search-container');
+const gallery = $('.gallary');
+const card;
+const cardImage;
+const cardInfo;
+const modalContainer;
+const modalButton;
+const modal; 
