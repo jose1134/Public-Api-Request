8ee06adcbe920ae4ff998fdadda99379cf8b3ba4
Jose Collado <jcoll1020@gmail.com>
2019-06-18 16:26:15 -0400

    new

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 const searchContainer = $('.search-container');
 const gallery = $('.gallary');
-const card;
-const cardImage;
-const cardInfo;
-const modalContainer;
-const modalButton;
-const modal; 
+// const card;
+// const cardImage;
+// const cardInfo;
+// const modalContainer;
+// const modalButton;
+// const modal; 
+
+
+function searchBox(){
+	searchContainer.html($('<form>', {action: '#', method: 'get'}));
+	$('form').html($('<input>', {type: 'search', id: "search-input", class: "search-input", placeholder: 'Search...'}));
+	$('form').append($('<input>', {type: 'submit', value: '&#x1F50D;',id: "search-submit", class: "search-submit"}));
+}
+
+
+searchContainer.append(searchBox);
