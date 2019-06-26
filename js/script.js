881bcb30285cb53ad302dccc0d90c047e6b388fe
Jose Collado <jcoll1020@gmail.com>
2019-06-26 17:01:12 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,47 +1,36 @@
 const searchContainer = $('.search-container');
-<<<<<<< HEAD
 const gallery = $('#gallery');
-=======
 const gallary = $('.gallary');
->>>>>>> bb68082d6b0e3751fc30e6b33afee62bfb7b6816
 const card = $('.card');
 const cardImageContainer = $(".card-img-container");
 const cardInfoContainer = $(".card-info-container");
 const cardImg = $(".card-img");
 const cardName = $(".card-name");
 const cardEmail = $("card-text");
-const cardCity = $(".card-text, .cap")
+const cardCity = $(".card-text, .cap");
 // const modalContainer;
 // const modalButton;
 // const modal; 
 
 
-function searchBox(){
+function newElements(){
 	searchContainer.html($('<form>', {action: '#', method: 'get'}));
 	$('form').html($('<input>', {type: 'search', id: "search-input", class: "search-input", placeholder: 'Search...'}));
 	$('form').append($('<input>', {type: 'submit', value: '&#x1F50D;',id: "search-submit", class: "search-submit"}));
+	
 }
-
-<<<<<<< HEAD
-function galleryMarkup(){
+function galleryElements(){
 	gallery.html($('<div>', {class: 'card'}));
-	card.html($('<div>', {class: "card-img-container"}));
-	card.append($('<div>', {class: "card-info-container"}));
-	cardImageContainer.html($('<img>', {class: 'card-img', src: "https://placehold.it/90x90", alt: "profile picture"}));
-	cardInfoContainer.html($('<h3>', {id: 'name', class: 'card-name cap'}));
-	cardInfoContainer.append($('<p>', {class: 'card-text cap'}));
-	cardCity.prepend($('<p>', {class: 'card-text'}));
+	$('.card').html($('<div>', {class: "card-img-container"}));
+	$('.card').append($('<div>', {class: "card-info-container"}));
+	$(".card-img-container").html($('<img>', {class: 'card-img', src: "https://placehold.it/90x90", alt: "profile picture"}));
+	$(".card-info-container").html($('<h3>', {id: 'name', class: 'card-name cap'}));
+	$(".card-info-container").append($('<p>', {class: 'card-text'}));
+	$(".card-info-container").append($('<p>', {class: 'card-text cap'}));
+	
 }
 
 
+newElements();
+galleryElements();
 
-galleryMarkup();
-
-
-=======
-function gallaryMarkup(){
-	gallary.html($('<div>'), {class: 'card'});
-	card.html($('<div>', {class: "card-img-container"}));
-	card.append($('<div>', {class: "card-info-container"}));
-}
->>>>>>> bb68082d6b0e3751fc30e6b33afee62bfb7b6816
