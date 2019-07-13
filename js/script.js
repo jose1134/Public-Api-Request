0c52bce4983f64ca2b31b19382159d92069a3685
Jose Collado <jcoll1020@gmail.com>
2019-07-13 03:16:43 -0400

    update

--- js/script.js
+++ js/script.js
@@ -6,13 +6,14 @@ function formElements(){
 	$('form').append($('<input>', {type: 'submit', value: '&#x1F50D;',id: "search-submit", class: "search-submit"}));
 	
 }
-function galleryElements(){
-	$('#gallery').html($('<div>', {class: 'card'}));
+function galleryElements(numOfBoxes){
+	for (let i = 0; i < numOfBoxes; i++){
+	$('#gallery').append($('<div>', {class: 'card'}));
 	$('.card').html($('<div>', {class: "card-img-container"}));
 	$('.card').append($('<div>', {class: "card-info-container"}));
 	$(".card-img-container").html($('<img>', {class: 'card-img', src: "https://placehold.it/90x90", alt: "profile picture"}));
 	$(".card-info-container").html($('<h3>', {id: 'name', class: 'card-name cap'}));
-	
+	}
 }
 
 function modalElements(){
@@ -37,8 +38,36 @@ function modalElements(){
 }
 
 formElements();
-galleryElements();
+galleryElements(10);
 modalElements();
+
+
+
+
+const userUrl = 'https://randomuser.me/api/?results=10';
+
+
+fetch(userUrl)
+	.then((resp) => resp.json())
+	.then(function(data){
+		let users = data.results;
+			return users.map(function(user){
+				$('.card-img, .modal-img').attr('src', user.picture.large);
+				$('.card-name, .modal-name').html(`${user.name.first} ${user.name.last}`);
+				$('<p>', {class: 'card-text'}).insertAfter(".card-name").html(`${user.email}`);
+				$('<p>', {class: 'card-text cap'}).insertAfter(".card-text").html(`${user.location.city}, ${user.location.state}`);
+				let modalTextClasses = $('p.modal-text');
+				for (let i = 0; i < modalTextClasses.length; i++){
+					modalTextClasses[0].innerHTML = `${user.email}`;
+					modalTextClasses[1].innerHTML = `${user.location.city}`;
+					modalTextClasses[2].innerHTML = `${user.cell}`;
+					modalTextClasses[3].innerHTML = `${user.location.street}, ${user.location.city}, ${user.location.state} ${user.location.postcode}`;
+					modalTextClasses[4].innerHTML = `Birthday: ${user.dob.date}`;
+				}
+			})	
+	})	
+	.catch(error => console.log(error));
+
 
 
 const modalContainer = $('.modal-container');
@@ -50,35 +79,6 @@ $('.card').click(function(event) {
 $('.modal-close-btn').click(function(event) {
 	modalContainer.fadeOut('slow');
 });
-
-const userUrl = 'https://randomuser.me/api/?results=1';
-
-
-fetch(userUrl)
-	.then((resp) => resp.json())
-	.then(function(data){
-		let users = data.results;
-		return users.map(function(users){
-			$('.card-img, .modal-img').attr('src', users.picture.large);
-			$('.card-name, .modal-name').html(`${users.name.first} ${users.name.last}`);
-			$('<p>', {class: 'card-text'}).insertAfter(".card-name").html(`${users.email}`);
-			$('<p>', {class: 'card-text cap'}).insertAfter(".card-text").html(`${users.location.city}, ${users.location.state}`);
-			let modalTextClasses = $('p.modal-text');
-			for (let i = 0; i < modalTextClasses.length; i++){
-				modalTextClasses[0].innerHTML = `${users.email}`;
-				modalTextClasses[1].innerHTML = `${users.location.city}`;
-				modalTextClasses[2].innerHTML = `${users.cell}`;
-				modalTextClasses[3].innerHTML = `${users.location.street}, ${users.location.city}, ${users.location.state} ${users.location.postcode}`;
-				modalTextClasses[4].innerHTML = `Birthday: ${users.dob.date}`;
-			}
-		
-		})
-	})	
-	.catch(error => console.log(error));
-
-
-
-
 
 
 
