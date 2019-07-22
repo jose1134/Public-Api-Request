8b88982df88cf1d5f4921defe26d92abc456b2c9
Jose Collado <jcoll1020@gmail.com>
2019-07-22 18:00:16 -0400

    update

--- js/src.js
+++ js/src.js
@@ -1,4 +1,4 @@
-const userUrl = 'https://randomuser.me/api/?results=10&nat=us,gb';
+const userUrl = 'https://randomuser.me/api/?results=10&nat=us';
 const gallery = $('#gallery');
 const searchContainer = $('.search-container');
 
@@ -13,3 +13,28 @@ function fetchAPI(url){
 Promise.all([fetchAPI(userUrl)])
   .then(data => data[0].results)
   .then(persons => persons.map(person => profiles(person)));
+
+
+
+
+function profiles(data){
+  gallery.append($('<div>', {class: 'card'}));
+  $('.card').html($('<div>', {class: "card-img-container"}));
+  $('.card').append($('<div>', {class: "card-info-container"}));
+  $(".card-img-container").html($('<img>', {class: 'card-img', src: "https://placehold.it/90x90", alt: "profile picture"}));
+  $(".card-info-container").html($('<h3>', {id: 'name', class: 'card-name cap'}));
+  $('.card-img, .modal-img').attr('src', data.picture.large);
+  $('.card-name, .modal-name').html(`${data.name.first} ${data.name.last}`);
+  $('<p>', {class: 'card-text'}).insertAfter(".card-name").html(`${data.email}`);
+  $('<p>', {class: 'card-text cap'}).insertAfter(".card-text").html(`${data.location.city}, ${data.location.state}`);
+}
+
+
+
+
+
+
+
+
+
+
