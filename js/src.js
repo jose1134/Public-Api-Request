7576acf63b2071f7e7dec17c6e689ce7e449e5cb
Jose Collado <jcoll1020@gmail.com>
2019-08-08 13:07:09 -0400

    update

--- js/src.js
+++ js/src.js
@@ -26,33 +26,37 @@ function createCards(data){
                         <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
                     </div>`;
     gallery.append(cardDiv);
+    $('.card').click(function(event) {
+      modalCards(employee, index);    });
   });
 
 
-
-  function modalCards(data){
-    data.map(function(employee, index) {
-      const modalContainer = document.createElement('div');
-      modalContainer.className = 'modal-container';
-      modalContainer.innerHTML = ` <div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                    <div class="modal-info-container">
-                        <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-                        <p class="modal-text">${employee.email}</p>
-                        <p class="modal-text cap">${employee.location.city}</p>
-                        <hr>
-                        <p class="modal-text">${employee.phone}</p>
-                        <p class="modal-text">${employee.location.street}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-                        <p class="modal-text">Birthday: ${employee.dob.date}</p>
-                    </div>
-                     <div class="modal-btn-container">
-                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
-                </div>`;
-      gallery.after(modalContainer);  
-    })
+  function modalCards(employee, index){
+    const modalContainer = document.createElement('div');
+    const birthdayDate = new Date(employee.dob.date)
+    modalContainer.className = 'modal-container';
+    modalContainer.innerHTML = ` <div class="modal">
+                  <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                  <div class="modal-info-container">
+                      <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+                      <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                      <p class="modal-text">${employee.email}</p>
+                      <p class="modal-text cap">${employee.location.city}</p>
+                      <hr>
+                      <p class="modal-text">${employee.phone}</p>
+                      <p class="modal-text">${employee.location.street}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                      <p class="modal-text">Birthday: ${birthdayDate.toLocaleDateString('en-US')}</p>
+                  </div>
+                   <div class="modal-btn-container">
+                  <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                  <button type="button" id="modal-next" class="modal-next btn">Next</button>
+              </div>`;
+    gallery.after(modalContainer); 
+    $('.modal-close-btn').click(function(event) {
+      $('.modal-container').remove();
+    }); 
   }
+
 }
 
 
